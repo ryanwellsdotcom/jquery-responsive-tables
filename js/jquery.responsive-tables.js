3e5e21b416a392038392f546d33e7d228f0336d7
Ryan Wells <ryanwells.com@gmail.com>
2015-12-06 14:23:36 -0500

    works reliably with combinations of merged cells now

--- js/jquery.responsive-tables.js
+++ js/jquery.responsive-tables.js
@@ -1,5 +1,5 @@
 /*
- * Responsive Tables plugin 1.0
+ * Responsive Tables plugin 1.1
  * Ryan Wells 
  * Copyright 2015, Ryan Wells (http://ryanwells.com)
  * Free to use under the MIT license.
@@ -9,40 +9,54 @@
     $.extend({
         responsiveTables: function(breakpoint) {
         	breakpoint = breakpoint || '800px';
-            // responsive tables
             if ($('.respond').not('responded').length > 0) {
-                $('.respond').not('responded').each(function(c) { // loop through each table that has .respond class
-                    c++; // start array at 1 for css instead of 0
-                    var className = 'instance-' + c; // add unique class name to each table with .respond class
-                    var $this = $(this); // query the DOM only once
-                    $this.addClass(className); // add the unique classes
-                    $this.addClass('responded'); // add the unique classes
-                    var respondHtml = '<style type="text/css">\n'; // write out scoped styles (ok in HTML5)
-                    respondHtml += '@media only screen and (max-width:'+breakpoint+')  {\n'; // change the width accordingly
-                    $this.find('thead th').each(function(i) { // loop through th tags 
-                        var $text = $(this).text(); // get table heading text from each th
-                        i++; // start array at 1 for css instead of 0
-                        // these will become the vertical table headers when collapsed for tablet and mobile
-                        respondHtml += '\t.' + className + '>tbody>tr>td:not([colspan]):nth-of-type(' + i + '):before { content: "' + $text + '"; }\n';
-                        respondHtml += '\t.' + className + '>tbody>tr>td.col-' + i + ':before { content: "' + $text + '"; }\n'; // applied to td tags on the row that has a colspan
-                    });
-                    $this.find('td[colspan]').each(function(i) { // loop through each cell with colspan
-                        i++; // one based index for css
-                        $(this).parent().find('td').each(function() { // loop through each td   
-                            var $td = $(this),
-                                colspan = parseInt($td.attr('colspan'), 10);
-                            $td.addClass('col-' + i); // add incremented class name to target cells
-                            if (!isNaN(colspan)) { // has colspan
-                                i += colspan; // increment index by colspan number
-                            } else {
-                                i++; // no colspan
+                $('.respond').not('responded').each(function(i) { 
+                    i++;
+                    var className = 'rt-instance-' + i; 
+                    var $this = $(this); 
+                    $this.addClass(className); 
+                    $this.addClass('responded'); 
+                    var respondHtml = '<style type="text/css">\n'; 
+                    respondHtml += '@media only screen and (max-width:'+breakpoint+')  {\n';
+                    var arrHeaderText = [];
+                    $this.find('thead th').each(function(i, $text) { 
+                        $text = $(this).text(); 
+                        i++; 
+                        arrHeaderText.push($text); 
+                        respondHtml += '\t.' + className + '>tbody>tr>td.rt-cell-' + i + ':before { content: "' + $text + '"; }\n';  
+                    });                    
+                    $this.find('tbody > tr').each(function(i) {
+                        var $this = $(this); 
+                        i++;
+                        var arrColspan = [];
+                        var modIndex = [];
+                        $this.find('td').each(function(i, c, m) {
+                            var $this = $(this);                            
+                            i++;
+                            if (modIndex > 0) {
+                                    modIndex[0];
+                                    i++;
+                                }
+                            if (arrColspan > 0) {
+                                m = (i + arrColspan.shift() - 1);
+                                modIndex.splice(0,1);
+                                modIndex.push(m);
+                                i = m;
+                            }                          
+                            if ($this.is('[colspan]')) {                
+                                c = parseInt($(this).prop('colspan'), 10);
+                                arrColspan.push(c);                                
                             }
-                        });
+                            if (i === arrHeaderText.length) {
+                                modIndex.splice(0,1);
+                            }
+                            $this.addClass('rt-cell-' + i);
+                        });                        
                     });
-                    respondHtml += '}\n'; // close media query 
-                    respondHtml += '</style>'; // end style
-                    $this.before(respondHtml); // insert styles above each corresponding table
-                });
+                    respondHtml += '}\n'; 
+                    respondHtml += '</style>'; 
+                    $this.before(respondHtml); 
+                }); 
             }
         }
     });
