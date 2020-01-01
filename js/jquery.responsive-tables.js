ad6d2cd2e3cac277f67c6af29f51ad4f1fd07d2d
Ryan Wells <ryanwells.com@gmail.com>
2020-01-01 01:56:10 -0500

    thead is now optional

--- js/jquery.responsive-tables.js
+++ js/jquery.responsive-tables.js
@@ -1,15 +1,15 @@
 /*
- * Responsive Tables plugin 2.0.0
- * Ryan Wells 
- * Copyright 2017, Ryan Wells (http://ryanwells.com)
+ * Responsive Tables plugin 2.1.0
+ * Ryan Wells
+ * Copyright 2019, Ryan Wells (https://ryanwells.com)
  * Free to use under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
-*/
+ */
 $.extend({
-    responsiveTables: function (breakpoint) {
+    responsiveTables: function(breakpoint) {
         breakpoint = breakpoint || '800px';
         if ($('table').length > 0) {
-            $('table').each(function (i) {
+            $('table').each(function(i) {
                 i++;
                 var className = 'jrt-instance-' + i;
                 var $this = $(this);
@@ -17,20 +17,33 @@ $.extend({
                 $this.addClass(className);
 
                 var respondHtml = '<style type="text/css">\n';
-                respondHtml += '@media only screen and (max-width:' + breakpoint + ')  {\n';
-                var arrHeaderText = [];
-                $this.find('thead th').each(function (i, $text) {
-                    $text = $(this).text();
-                    i++;
-                    arrHeaderText.push($text);
-                    respondHtml += '\t.' + className + '>tbody>tr>td.jrt-cell-' + i + ':before { content: "' + $text + '"; }\n';
-                });
-                $this.find('tbody > tr').each(function (i) {
+                respondHtml +=
+                    '@media only screen and (max-width:' +
+                    breakpoint +
+                    ')  {\n';
+                if ($this.find('thead').length > 0) {
+                    var arrHeaderText = [];
+                    $this.find('thead th').each(function(i, $text) {
+                        $text = $(this).text();
+                        i++;
+                        arrHeaderText.push($text);
+                        respondHtml +=
+                            '\t.' +
+                            className +
+                            '>tbody>tr>td.jrt-cell-' +
+                            i +
+                            ':before { content: "' +
+                            $text +
+                            '"; }\n';
+                    });
+                    respondHtml += '\t.jrt td { padding-left: 50% }\n';
+                }
+                $this.find('tbody > tr').each(function(i) {
                     var $this = $(this);
                     i++;
                     var arrColspan = [];
                     var modIndex = [];
-                    $this.find('td').each(function (i, c, m) {
+                    $this.find('td').each(function(i, c, m) {
                         var $this = $(this);
                         i++;
                         if (modIndex > 0) {
@@ -38,7 +51,7 @@ $.extend({
                             i++;
                         }
                         if (arrColspan > 0) {
-                            m = (i + arrColspan.shift() - 1);
+                            m = i + arrColspan.shift() - 1;
                             modIndex.splice(0, 1);
                             modIndex.push(m);
                             i = m;
@@ -50,9 +63,11 @@ $.extend({
                         $this.addClass('jrt-cell-' + i);
                     });
                 });
-                respondHtml += '}\n';
-                respondHtml += '</style>';
-                $this.before(respondHtml);
+                if ($this.find('thead').length > 0) {
+                    respondHtml += '}\n';
+                    respondHtml += '</style>';
+                    $this.before(respondHtml);
+                }
             });
         }
     }
